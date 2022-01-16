77514f84e14900433d220aaca990f7bc87c3e832
theCoderSchool Syosset <macstation2@theCodeolsMini2.fios-router.home>
2022-01-16 12:56:12 -0500

    Added todo list with java script

---
+++ main.js
@@ -0,0 +1,25 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+
+todoButton.addEventListener("click", addTodo);
+
+
+function addTodo(event){
+    event.preventDefault();
+    console.log("The Button Works")
+
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    todoList.appendChild(todoDiv);
+
+
+}
